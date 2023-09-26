884a2337787c644996ffc86b8d77e1a004229203
Danilop109 <msdaniela109@gmail.com>
2023-09-26 15:43:18 -0500

    feat: :construction: IUnitOfWork
    Creacion de la interfas de unidad de trabajo

--- Aplicacion/Repositorio/CiudadRepository.cs
+++ Aplicacion/Repositorio/CiudadRepository.cs
@@ -2,11 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Dominio.Entities;
+using Dominio.Interfaces;
+using Persistencia;
 
 namespace Aplicacion.Repositorio
 {
-    public class CiudadRepository
+    public class CiudadRepository : GenericRepository<Ciudad>, ICiudad
     {
-        
+        public CiudadRepository(ApiFarmaciaContext context) : base(context)
+        {
+        }
     }
 }
