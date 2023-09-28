d90746c5d778c009d59b91bee5ea2ed383af7b3a
Danilop109 <msdaniela109@gmail.com>
2023-09-27 22:17:25 -0500

    feat: :construction: controlladores

--- ApiFarmacia/Controllers/TipoContactoController.cs
+++ ApiFarmacia/Controllers/TipoContactoController.cs
@@ -3,83 +3,99 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using ApiFarmacia.Dtos;
+using AutoMapper;
 using Dominio.Entities;
 using Dominio.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
-namespace ApiFarmacia.Controllers;
+namespace ApiFarmacia.Controllers
+{
     public class TipoContactoController : BaseApiController
     {
         private readonly IUnitOfWork unitOfWork;
-        
-                public TipoContactoController(IUnitOfWork unitOfWork)
-                {
-                    this.unitOfWork =unitOfWork;
-                }
-        
-                [HttpGet]
-                [ProducesResponseType(StatusCodes.Status200OK)]
-                [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        
-                public async Task<ActionResult<IEnumerable<TipoContacto>>> Get()
-                {
-                    var llamado = await unitOfWork.TipoContactos.GetAllAsync();
-                    return Ok(llamado);
-                }
-        
-                [HttpGet("{id}")]
-                [ProducesResponseType(StatusCodes.Status200OK)]
-                [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        
-                public async Task<ActionResult> Get(int id)
-                {
-                    var llamado = await unitOfWork.TipoContactos.GetByIdAsync(id);
-                    return Ok(llamado);
-                }
-        
-                [HttpPost]
-                [ProducesResponseType(StatusCodes.Status201Created)]
-                [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        
-                public async Task<ActionResult<TipoContacto>> Post(TipoContacto tipoContacto)
-                {
-                    this.unitOfWork.TipoContactos.Add(tipoContacto);
-                    await unitOfWork.SaveAsync();
-                    if (tipoContacto == null)
-                    {
-                        return BadRequest();
-                    }
-                    return CreatedAtAction(nameof(Post), new { id = tipoContacto.Id }, tipoContacto);
-                }
-        
-                [HttpPut("{id}")]
-                [ProducesResponseType(StatusCodes.Status200OK)]
-                [ProducesResponseType(StatusCodes.Status404NotFound)]
-                [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        
-                public async Task<ActionResult<TipoContacto>> Put(int id,[FromBody] TipoContacto tipoContacto)
-                {
-                    if(tipoContacto == null)
-                    return NotFound();
-                    unitOfWork.TipoContactos.Update(tipoContacto);
-                    await unitOfWork.SaveAsync();
-                    return tipoContacto;
-                }
-        
-                [HttpDelete("{id}")]
-                [ProducesResponseType(StatusCodes.Status200OK)]
-                [ProducesResponseType(StatusCodes.Status404NotFound)]
-                [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        
-                public async Task<IActionResult> Delete(int id)
-                { 
-                    var llamado = await unitOfWork.TipoContactos.GetByIdAsync(id);
-                    if (llamado == null){
-                        return NotFound();
-                    }
-                    unitOfWork.TipoContactos.Remove(llamado);
-                    await unitOfWork.SaveAsync();
-                    return NoContent();
-                }
+        private readonly IMapper mapper;
+
+        public TipoContactoController(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            this.unitOfWork = unitOfWork;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+
+        public async Task<ActionResult<IEnumerable<TipoContactoDto>>> Get()
+        {
+            var llamado = await unitOfWork.TipoContactos.GetAllAsync();
+            return mapper.Map<List<TipoContactoDto>>(llamado);
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        public async Task<ActionResult<TipoContactoDto>> Get(int id)
+        {
+            var llamado = await unitOfWork.TipoContactos.GetByIdAsync(id);
+            if (llamado == null)
+            {
+                return NotFound();
+            }
+            return mapper.Map<TipoContactoDto>(llamado);
+        }
+
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+
+        public async Task<ActionResult<TipoContacto>> Post(TipoContactoDto tipoContactoDto)
+        {
+            var llamado = this.mapper.Map<TipoContacto>(tipoContactoDto);
+            this.unitOfWork.TipoContactos.Add(llamado);
+            await unitOfWork.SaveAsync();
+            if (llamado == null)
+            {
+                return BadRequest();
+            }
+            return CreatedAtAction(nameof(Post), new { id = tipoContactoDto.Id }, tipoContactoDto);
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+
+        public async Task<ActionResult<TipoContactoDto>> Put(int id, [FromBody] TipoContactoDto tipoContactoDto)
+        {
+            if (tipoContactoDto == null)
+                return NotFound();
+            var llamado = mapper.Map<TipoContacto>(tipoContactoDto);
+            unitOfWork.TipoContactos.Update(llamado);
+            await unitOfWork.SaveAsync();
+            return tipoContactoDto;
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var llamado = await unitOfWork.TipoContactos.GetByIdAsync(id);
+            if (llamado == null)
+            {
+                return NotFound();
+            }
+            unitOfWork.TipoContactos.Remove(llamado);
+            await unitOfWork.SaveAsync();
+            return NoContent();
+        }
     }
+}
+
--- Aplicacion/Repositorio/TipoDocumentoRepository.cs
+++ Aplicacion/Repositorio/TipoDocumentoRepository.cs
@@ -4,14 +4,30 @@ using System.Linq;
 using System.Threading.Tasks;
 using Dominio.Entities;
 using Dominio.Interfaces;
+using Microsoft.EntityFrameworkCore;
 using Persistencia;
 
 namespace Aplicacion.Repositorio
 {
     public class TipoDocumentoRepository : GenericRepository<TipoDocumento>, ITipoDocumento
     {
+        private readonly ApiFarmaciaContext _context;
+
         public TipoDocumentoRepository(ApiFarmaciaContext context) : base(context)
         {
+            _context = context;
+        }
+
+        public override async Task<IEnumerable<TipoDocumento>> GetAllAsync()
+        {
+            return await _context.TipoDocumentos
+                .ToListAsync();
+        }
+
+        public override async Task<TipoDocumento> GetByIdAsync(int id)
+        {
+            return await _context.TipoDocumentos
+            .FirstOrDefaultAsync(p => p.Id == id);
         }
     }
 }
